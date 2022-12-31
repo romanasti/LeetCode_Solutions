5b2f66a23914712318f1e0d4b4767ded0797575f
Roman_Chub <rchub@inbox.ru>
2023-01-01 02:16:39 +0300

    Count Negative Numbers in a Sorted Matrix is done

---
+++ Easy/Count_Negative_Numbers_in_a_Sorted_Matrix/Solution.js
@@ -0,0 +1,13 @@
+var countNegatives = function(grid) {
+let res = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if(grid[i][j] < 0) {
+                res ++
+            }
+        }
+    }
+    return res
+};
+
+console.log(countNegatives([[3,2],[1,0]]))
