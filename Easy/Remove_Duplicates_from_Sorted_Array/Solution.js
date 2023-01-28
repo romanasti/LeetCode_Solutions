5901384d3832cc93c64d70fdba1d32f4c3f5151c
Roman_Chub <rchub@inbox.ru>
2023-01-29 01:53:56 +0300

    another solution is added

--- Easy/Remove_Duplicates_from_Sorted_Array/Solution.js
+++ Easy/Remove_Duplicates_from_Sorted_Array/Solution.js
@@ -12,3 +12,18 @@ let j = 1;
 
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
 
+// another solution
+// const arr1 = [0,0,1,1,1,2,2,3,3,4]
+//
+// let removeDuplicates = function (nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i] === nums[i-1]) {
+//             nums.splice(i,1)
+//             i--
+//         }
+//     }
+//     return nums.length
+// }
+//
+// console.log(removeDuplicates(arr1))
+// console.log(arr1)
