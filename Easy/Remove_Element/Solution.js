b7b10dda0dc4b4128a3b76150b80c7cf14e4a5b0
Roman_Chub <rchub@inbox.ru>
2023-01-29 02:04:06 +0300

    Remove Element is done

---
+++ Easy/Remove_Element/Solution.js
@@ -0,0 +1,15 @@
+arr1 = [0, 1, 2, 2, 3, 0, 4, 2];
+num = 2;
+
+var removeElement = function (nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+    return nums.length
+};
+
+console.log(removeElement(arr1, num))
+console.log(arr1)
