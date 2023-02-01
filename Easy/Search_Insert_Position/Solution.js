6ce5cac7e0743bd5e804949f0869c5d1f2ad0c76
Roman_Chub <rchub@inbox.ru>
2023-02-02 01:51:48 +0300

    solved problem with complexity 0(n)

---
+++ Easy/Search_Insert_Position/Solution.js
@@ -0,0 +1,13 @@
+
+// runtime complexity 0(n);
+var searchInsert = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+        return i
+        }
+    }
+    return nums.length
+};
+
+console.log(searchInsert([1, 3, 5, 6], 7))
+
