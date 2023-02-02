b0f245821fa126eab3fb70686f734f8aa386cfcc
Roman_Chub <rchub@inbox.ru>
2023-02-03 02:56:09 +0300

    added solution with runtime complexity 0(log n) - binary search

--- Easy/Search_Insert_Position/Solution.js
+++ Easy/Search_Insert_Position/Solution.js
@@ -1,13 +1,36 @@
+// runtime complexity 0(log n);
+// Binary search
 
-// runtime complexity 0(n);
 var searchInsert = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target) {
-        return i
+    let left = 0;
+    let right = nums.length;
+    let mid;
+
+    while (left < right) {
+        mid = Math.floor((right + left) / 2);
+        if (target === nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            right = mid;
+        } else {
+            left = mid + 1;
         }
     }
-    return nums.length
+    return left;
 };
 
-console.log(searchInsert([1, 3, 5, 6], 7))
+console.log(searchInsert([1, 3, 5, 6], 5))
+
+
+// runtime complexity 0(n);
+// var searchInsert = function (nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] >= target) {
+//         return i
+//         }
+//     }
+//     return nums.length
+// };
+//
+// console.log(searchInsert([1, 3, 5, 6], 7))
 
