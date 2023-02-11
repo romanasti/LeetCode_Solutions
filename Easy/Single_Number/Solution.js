6ce54d44ae2f30ad4fbe6f59de812b3d01321933
Roman_Chub <rchub@inbox.ru>
2023-02-12 01:40:05 +0300

    Single Number is done

---
+++ Easy/Single_Number/Solution.js
@@ -0,0 +1,10 @@
+var singleNumber = function (nums) {
+
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        res ^= nums[i]
+    }
+    return res
+};
+
+console.log(singleNumber([2, 2, 1]))
