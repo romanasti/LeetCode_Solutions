275366ce7971b4b5dd499f88506283db6cb49e4d
Roman_Chub <rchub@inbox.ru>
2023-02-14 23:34:11 +0300

    Fibonacci Number is done

---
+++ Easy/Fibonacci_Number/Solution.js
@@ -0,0 +1,13 @@
+var fib = function(n) {
+    if(n === 0) return 0
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b
+};
+
+console.log(fib(0))
